96fed233a59b20ab3b9d8937b94073a627ecd457
Pikin <pikin@bk.ru>
2023-09-14 17:56:12 +0500

    last task fixed

--- src/Solid/Fuel/Engine.java
+++ src/Solid/Fuel/Engine.java
@@ -1,16 +1,11 @@
 package Solid.Fuel;
 
-public class Engine implements PetrolEngine{
+public class Engine {
     String engineType;
-    public Engine(){
-        this.engineType = setEngine();
+    public Engine(String engineType){
+        this.engineType = engineType;
     }
     public void start(){
         System.out.println("Машина заведена");
-    }
-
-    @Override
-    public String setEngine() {
-        return PetrolEngine.engineType;
     }
 }
