dd25a39d23b8873484a7fb54996ae3cf983b1e8d
Evgeniy Pikin <pikin@bk.ru>
2023-09-18 21:52:54 +0500

    complex class added

--- src/Patterns/ComplexCalculator/Complex.java
+++ src/Patterns/ComplexCalculator/Complex.java
@@ -6,15 +6,24 @@ public class Complex {
     Scanner sc = new Scanner(System.in);
     double real;
     double image;
+
     public Complex() {
-        double real = sc.nextDouble();
-        double image = sc.nextDouble();
-        sc.close();
+        String stringNum = sc.nextLine();
+//        stringNum = stringNum.substring(0,stringNum.length()-2);
+        double real = RealComplexFromString(stringNum);
+        double image = ImageComplexFromString(stringNum);
         Complex(real,image);
     }
     private void Complex(double real, double image){
         this.real = real;
         this.image = image;
     }
-
+    private double RealComplexFromString(String tempStringNum){
+        tempStringNum = tempStringNum.substring(0, tempStringNum.indexOf('+'));
+        return Double.parseDouble(tempStringNum);
+    }
+    private double ImageComplexFromString(String tempStringNum){
+        tempStringNum = tempStringNum.substring(tempStringNum.indexOf('+')+1, tempStringNum.length()-1);
+        return Double.parseDouble(tempStringNum);
+    }
 }
--- src/Patterns/ComplexCalculator/ViewCalculator.java
+++ src/Patterns/ComplexCalculator/ViewCalculator.java
@@ -5,12 +5,16 @@ import java.util.Scanner;
 public class ViewCalculator {
 
     private Complex primary;
+    private Complex secondary;
     public void run(){
         while (true){
 
             System.out.println("Введите первое комплексное число: ");
             primary = new Complex();
+            System.out.println("Введите второе комплексное число: ");
+            secondary = new Complex();
             System.out.println(primary.real + " " + primary.image);
+            System.out.println(secondary.real + " " + secondary.image);
             break;
 
 
